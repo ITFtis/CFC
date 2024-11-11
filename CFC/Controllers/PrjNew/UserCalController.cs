518a77ba1177e114def413f2b38587bee4490165
brianlin12345 <brianlin12345@gamil.com>
2024-11-11 16:07:24 +0800

    會員統計數據 v1

---
+++ CFC/Controllers/PrjNew/UserCalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CFC.Controllers.PrjNew
+{
+    [Dou.Misc.Attr.MenuDef(Id = "UserCal", Name = "會員統計數據", MenuPath = "管理", Action = "Index", Index = 1, Func = Dou.Misc.Attr.FuncEnum.ALL, AllowAnonymous = false)]
+    public class UserCalController : Controller
+    {
+        // GET: UserCal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
