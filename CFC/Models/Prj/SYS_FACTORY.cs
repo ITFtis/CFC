23305b201cccd09c0be891b679332542b6598772
brianlin12345 <brianlin12345@gamil.com>
2024-11-27 10:06:07 +0800

    管理 公司

--- CFC/Models/Prj/SYS_FACTORY.cs
+++ CFC/Models/Prj/SYS_FACTORY.cs
@@ -19,47 +19,49 @@ namespace CFC.Models.Prj
         /// <summary>
         /// 主鍵，自動遞增的工廠編號
         /// </summary>
-        //[Key]
-        //[Display(Name = "主鍵，自動遞增的工廠編號", Order = 0)]
-        //[ColumnDef(Visible = false, VisibleEdit = false)]
-        //[ColumnDef(Visible = false)]
-        //public string FACTORY_ID { get; set; }
+        [Key]
+        [Display(Name = "編號", Order = 0)]
+        [ColumnDef(Visible = false, VisibleEdit = false)]
+        public int FACTORY_ID { get; set; }
 
-        [Display(Name = "工廠名稱", Order = 0)]
+        [Display(Name = "工廠名稱")]
         [ColumnDef(Visible = true, VisibleEdit = true)]
         public string FACTORY_NAME { get; set; }
 
-        [Key]
-        [Display(Name = "登記證", Order = 2)]
+        [Display(Name = "登記證")]
         //[ColumnDef(Filter = true, FilterAssign = FilterAssignType.Contains)]
         public string FACTORY_REGISTRATION { get; set; }
 
-        [Display(Name = "縣市別", Order = 3)]
+        [Display(Name = "縣市別")]
         public string FACTORY_CITY { get; set; }
 
 
-        [Display(Name = "鄉鎮市區", Order = 4)]
+        [Display(Name = "鄉鎮市區")]
         public string FACTORY_DISTRICT { get; set; }
 
-        [Display(Name = "地址", Order = 5)]
+        [Display(Name = "地址")]
         public string FACTORY_ADDRESS { get; set; }
 
-        [Display(Name = "工業區", Order = 6)]
+        [Display(Name = "工業區")]
         public string FACTORY_INDUSTRIAL_AREA { get; set; }
 
-        [Display(Name = "工廠產業類型", Order = 7)]
+        [Display(Name = "工廠產業類型")]
         public string FACTORY_INDUSTRIAL { get; set; }
 
-        [Display(Name = "建檔日", Order = 8)]
+        [Display(Name = "建檔日")]
+        [ColumnDef(Visible = false, VisibleEdit = false)]
         public string BDate { get; set; }
 
-        [Display(Name = "建檔者ID", Order = 9)]
+        [Display(Name = "建檔者ID")]
+        [ColumnDef(Visible = false, VisibleEdit = false)]
         public string BId { get; set; }
 
-        [Display(Name = "修改日", Order = 10)]
+        [Display(Name = "修改日")]
+        [ColumnDef(Visible = false, VisibleEdit = false)]
         public string UDate { get; set; }
 
-        [Display(Name = "修改者ID", Order = 11)]
+        [Display(Name = "修改者ID")]
+        [ColumnDef(Visible = false, VisibleEdit = false)]
         public string UId { get; set; }
 
 
