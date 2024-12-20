bc1f1287fac33f3b67aee5914d8b6f4ab3ec4568
brianlin12345 <brianlin12345@gamil.com>
2024-12-20 10:13:29 +0800

    script

--- CFC/Docs/UpdateScript/20241220_會員建立日期.sql
+++ CFC/Docs/UpdateScript/20241220_會員建立日期.sql
@@ -4,4 +4,10 @@ alter Table User_Properties_Advance add [BDate] [nvarchar](14) NULL
 alter Table User_Properties_Advance add [BId] [nvarchar](100) NULL
 alter Table User_Properties_Advance add [UDate] [nvarchar](14) NULL
 alter Table User_Properties_Advance add [UId] [nvarchar](100) NULL
+Go
 
+Update User_Properties_Advance
+Set BDate = '2024/09/01',
+    BId = Id
+
+Go
