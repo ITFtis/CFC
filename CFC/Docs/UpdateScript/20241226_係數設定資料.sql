2f0c8ba86e633c76a7a94e0c24cd03c3413a5926
brianlin12345 <brianlin12345@gamil.com>
2024-12-26 09:46:11 +0800

    script 係數設定資料 2

--- CFC/Docs/UpdateScript/20241226_係數設定資料.sql
+++ CFC/Docs/UpdateScript/20241226_係數設定資料.sql
@@ -1,11 +1,33 @@
 --係數設定資料
 
---alter TABLE xxxxxx ALTER COLUMN ooooooooo
+--調整(Fuel_properties)
+alter TABLE Fuel_properties ALTER COLUMN [CO2] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [CH4] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [NO2] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GCO2R4] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GCH4R4] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GNO2R4] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GCO2R5] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GCH4R5] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GNO2R5] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GCO2R6] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GCH4R6] [decimal](15, 10) NOT NULL
+alter TABLE Fuel_properties ALTER COLUMN [GNO2R6] [decimal](15, 10) NOT NULL
+Go
 
-Select * From Fuel_properties
-Select * From Elec_properties
-Select * From Specific_properties
-Select * From Escape_properties
+--調整(Elec_properties)
+alter TABLE Elec_properties ALTER COLUMN [Co2e] [decimal](15, 10) NOT NULL
+Go
+
+--調整(Specific_properties)	
+alter TABLE Specific_properties ALTER COLUMN [CO2] [decimal](15, 10) NULL
+alter TABLE Specific_properties ALTER COLUMN [CH4] [decimal](15, 10) NULL
+alter TABLE Specific_properties ALTER COLUMN [N2O] [decimal](15, 10) NULL
+alter TABLE Specific_properties ALTER COLUMN [HFCs] [decimal](15, 10) NULL
+alter TABLE Specific_properties ALTER COLUMN [PFCs] [decimal](15, 10) NULL
+alter TABLE Specific_properties ALTER COLUMN [SF6] [decimal](15, 10) NULL
+alter TABLE Specific_properties ALTER COLUMN [NF3] [decimal](15, 10) NULL
+Go
 
 --調整(Escape_properties)
 alter TABLE Escape_properties ALTER COLUMN [CO2] [decimal](15, 10) NULL
@@ -15,3 +37,5 @@ alter TABLE Escape_properties ALTER COLUMN [HFCs] [decimal](15, 10) NULL
 alter TABLE Escape_properties ALTER COLUMN [PFCs] [decimal](15, 10) NULL
 alter TABLE Escape_properties ALTER COLUMN [SF6] [decimal](15, 10) NULL
 alter TABLE Escape_properties ALTER COLUMN [NF3] [decimal](15, 10) NULL
+Go
+
